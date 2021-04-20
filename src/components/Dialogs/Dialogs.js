8d07b3f15289ae570f79ba08ed97997b54d66973
Denis Frolov <denisfrolov.coder@gmail.com>
2021-04-21 01:08:15 +0300

    add ref in dialogs

--- src/components/Dialogs/Dialogs.js
+++ src/components/Dialogs/Dialogs.js
@@ -1,21 +1,35 @@
 import React from "react";
-import Dialog from './Dialog/Dialog';
-import Message from './Message/Message';
+import Dialog from "./Dialog/Dialog";
+import Message from "./Message/Message";
 import styles from "./Dialogs.module.css";
 
 const Dialogs = (props) => {
+
+  const text = React.createRef();
+  const btnSend = React.createRef();
 
   return (
     <div className={styles.dialogs}>
       <div className={styles.dialogs_items}>
         {props.dialogsData.map((dialog) => (
-          <Dialog key={dialog.id} id={dialog.id} name={dialog.name} url={dialog.url}/>
+          <Dialog
+            key={dialog.id}
+            id={dialog.id}
+            name={dialog.name}
+            url={dialog.url}
+          />
         ))}
       </div>
       <div className={styles.messages}>
-        {props.messagesData.map((message) => (
-          <Message key={message.id} message={message.message} />
-        ))}
+        <div className={styles.messages__block}>
+          {props.messagesData.map((message) => (
+            <Message key={message.id} message={message.message} />
+          ))}
+        </div>
+        <div className={styles.messages__input}>
+          <input ref={text} className={styles.text__input} type="text" placeholder="your text..." />
+          <input ref={btnSend} onClick={() => {alert(text.current.value)}} className={styles.btn__send} type="button" value="send"/>
+        </div>
       </div>
     </div>
   );
